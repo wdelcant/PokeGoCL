fe6d95a24741e4636e70ecab3933ad71c3e0db9d
Wilson del canto cepeda <wilsondelcanto.redes@gmail.com>
2022-08-08 13:17:15 -0400

    Se agregan animaciones

--- js/script.js
+++ js/script.js
@@ -20,12 +20,17 @@
 	});
 
 
-	new WOW().init();
-	// wow = new WOW({
-	// 	boxClass: 'wow', // default
-	// 	animateClass: 'animated', // default
-	// 	offset: 0, // default
-	// 	mobile: true, // default
-	// 	live: true // defaultz
-	// })
-	// wow.init();
+	const wow = new WOW({
+		boxClass: 'wow', // animated element css class (default is wow)
+		animateClass: 'animated', // animation css class (default is animated)
+		offset: 0, // distance to the element when triggering the animation (default is 0)
+		mobile: true, // trigger animations on mobile devices (default is true)
+		live: true, // act on asynchronously loaded content (default is true)
+		callback: function (box) {
+			// the callback is fired every time an animation is started
+			// the argument that is passed in is the DOM node being animated
+		},
+		scrollContainer: null, // optional scroll container selector, otherwise use window,
+		resetAnimation: true, // reset animation on end (default is true)
+	});
+	wow.init();
