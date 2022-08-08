87f293ed8d055ecf8f52ff7fb8128a3c0e07799a
Wilson del canto cepeda <wilsondelcanto.redes@gmail.com>
2022-08-08 11:52:12 -0400

    fixes

--- js/script.js
+++ js/script.js
@@ -1,19 +1,23 @@
 	// loader
 	window.addEventListener("load", function () {
-	    const loader = document.querySelector(".loader");
-	    loader.className += "hidden";
+		const loader = document.querySelector(".loader");
+		loader.className += "hidden";
 	});
 
 
 	// register
 	const register = document.querySelector(".register");
 	register.addEventListener("click", function () {
-	    //sweetAlert2
-	    Swal.fire({
-	        icon: 'error',
-	        title: 'Uyss...',
-	        text: 'Aun no se habilitan los registros!',
-	        footer: '<a href>Contacta con el administrador</a>'
-	    })
+		//sweetAlert2
+		Swal.fire({
+			icon: 'error',
+			title: 'Uyss...',
+			text: 'Aun no se habilitan los registros!',
+			footer: '<a href="mailto:soporte@pokegocl.com?Subject=Contacto%20web%20PokeGOCL">Contacta al administrador</a>'
+		})
 
 	});
+
+
+	// AOS animation
+	AOS.init();
